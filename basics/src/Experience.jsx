a30964bf51dbb0e10183b8bb5ce0952275831725
Thabish <kadertabish2@gmail.com>
2023-02-11 10:50:09 +0400

    animate
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- basics/src/Experience.jsx
+++ basics/src/Experience.jsx
@@ -3,25 +3,28 @@ import React, { useRef } from "react";
 
 export const Experience = () => {
 	const cubeRef = useRef(null);
-	useFrame(() => {
-		cubeRef.current.rotation.y += 0.01;
+	const groupRef = useRef(null);
+	useFrame((state, delta) => {
+		cubeRef.current.rotation.y += delta;
 	});
 	return (
 		<>
-			<mesh position-x={-2}>
-				<sphereGeometry />
-				<meshBasicMaterial color="orange" />
-			</mesh>
+			<group ref={groupRef}>
+				<mesh position-x={-2}>
+					<sphereGeometry />
+					<meshBasicMaterial color="orange" />
+				</mesh>
 
-			<mesh
-				ref={cubeRef}
-				rotation-y={Math.PI * 0.25}
-				position-x={2}
-				scale={1.5}
-			>
-				<boxGeometry />
-				<meshBasicMaterial color="mediumpurple" />
-			</mesh>
+				<mesh
+					ref={cubeRef}
+					rotation-y={Math.PI * 0.25}
+					position-x={2}
+					scale={1.5}
+				>
+					<boxGeometry />
+					<meshBasicMaterial color="mediumpurple" />
+				</mesh>
+			</group>
 
 			<mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
 				<planeGeometry />
