28c6c9487f3cbb402eaa776d2a4e84cba370d126
Thabish <kadertabish2@gmail.com>
2023-02-14 15:25:34 +0400

    portal
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- portal/src/Experience.jsx
+++ portal/src/Experience.jsx
@@ -1,15 +1,34 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
+import { useGLTF, useTexture, Center } from "@react-three/drei";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+	const { nodes } = useGLTF("./model/portal.glb");
+	const bakedTexture = useTexture("./model/baked.jpg");
+	bakedTexture.flipY = false;
+	console.log(nodes);
+	return (
+		<>
+			<color args={["#030202"]} attach="background" />
+			<OrbitControls makeDefault />
+			<Center>
+				<mesh geometry={nodes.baked.geometry}>
+					<meshBasicMaterial map={bakedTexture} />
+				</mesh>
 
-        <OrbitControls makeDefault />
+				<mesh
+					position={nodes.poleLightA.position}
+					geometry={nodes.poleLightA.geometry}
+				>
+					<meshBasicMaterial color="#ffffe5" />
+				</mesh>
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
-    </>
+				<mesh
+					position={nodes.poleLightB.position}
+					geometry={nodes.poleLightB.geometry}
+				>
+					<meshBasicMaterial color="#ffffe5" />
+				</mesh>
+			</Center>
+		</>
+	);
 }
