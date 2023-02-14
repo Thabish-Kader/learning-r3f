5dbc8c685edf18f67443393803a2879cc3e795ab
Thabish <kadertabish2@gmail.com>
2023-02-14 15:55:23 +0400

    sparkeles
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- portal/src/Experience.jsx
+++ portal/src/Experience.jsx
@@ -1,11 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
-import { useGLTF, useTexture, Center } from "@react-three/drei";
+import { useGLTF, useTexture, Center, Sparkles } from "@react-three/drei";
 
 export default function Experience() {
 	const { nodes } = useGLTF("./model/portal.glb");
 	const bakedTexture = useTexture("./model/baked.jpg");
 	bakedTexture.flipY = false;
-	console.log(nodes);
+
 	return (
 		<>
 			<color args={["#030202"]} attach="background" />
@@ -28,6 +28,13 @@ export default function Experience() {
 				>
 					<meshBasicMaterial color="#ffffe5" />
 				</mesh>
+
+				<mesh
+					geometry={nodes.portalLight.geometry}
+					position={nodes.portalLight.position}
+					rotation={nodes.portalLight.rotation}
+				></mesh>
+				<Sparkles position={[0, 1, 0]} scale={3} size={6} />
 			</Center>
 		</>
 	);
