107366c146970254122a29c054884c4226214bc2
Thabish <kadertabish2@gmail.com>
2023-02-14 16:35:31 +0400

    portal-done
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- portal/src/Experience.jsx
+++ portal/src/Experience.jsx
@@ -1,11 +1,39 @@
-import { OrbitControls } from "@react-three/drei";
-import { useGLTF, useTexture, Center, Sparkles } from "@react-three/drei";
+import * as THREE from "three";
+import { extend, useFrame } from "@react-three/fiber";
 
+import {
+	useGLTF,
+	useTexture,
+	Center,
+	Sparkles,
+	shaderMaterial,
+	OrbitControls,
+} from "@react-three/drei";
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
+import { useRef } from "react";
+
+const PortalMaterial = shaderMaterial(
+	{
+		uTime: 0,
+		uColorStart: new THREE.Color("#ffffff"),
+		uColorEnd: new THREE.Color("#000000"),
+	},
+	portalVertexShader,
+	portalFragmentShader
+);
+extend({
+	PortalMaterial,
+});
 export default function Experience() {
 	const { nodes } = useGLTF("./model/portal.glb");
+	const portalMaterialRef = useRef();
+
 	const bakedTexture = useTexture("./model/baked.jpg");
 	bakedTexture.flipY = false;
-
+	useFrame((state, delta) => {
+		portalMaterialRef.current.uTime += delta;
+	});
 	return (
 		<>
 			<color args={["#030202"]} attach="background" />
@@ -33,8 +61,25 @@ export default function Experience() {
 					geometry={nodes.portalLight.geometry}
 					position={nodes.portalLight.position}
 					rotation={nodes.portalLight.rotation}
-				></mesh>
-				<Sparkles position={[0, 1, 0]} scale={3} size={6} />
+				>
+					{/* <shaderMaterial
+						vertexShader={portalVertexShader}
+						fragmentShader={portalFragmentShader}
+						uniforms={{
+							uTime: { value: 0 },
+							uColorStart: { value: new THREE.Color("#ffffff") },
+							uColorEnd: { value: new THREE.Color("#000000") },
+					/>
+						}} */}
+					<portalMaterial ref={portalMaterialRef} />
+				</mesh>
+				<Sparkles
+					position={[0, 1, 0]}
+					scale={[4, 2, 4]}
+					size={6}
+					speed={0.2}
+					count={41}
+				/>
 			</Center>
 		</>
 	);
