20da30fe7b41e0faac71e67f35a748c29446a30a
Thabish <kadertabish2@gmail.com>
2023-02-15 18:45:09 +0400

    physics
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- physics/src/Experience.jsx
+++ physics/src/Experience.jsx
@@ -1,31 +1,48 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { CuboidCollider, Debug, RigidBody, Physics } from "@react-three/rapier";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+	return (
+		<>
+			<Perf position="top-left" />
 
-        <Perf position="top-left" />
+			<OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
+			<directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+			<ambientLight intensity={0.5} />
+			<Physics>
+				<Debug />
+				<RigidBody colliders="ball">
+					<mesh castShadow position={[0, 4, 0]}>
+						<sphereGeometry />
+						<meshStandardMaterial color="orange" />
+					</mesh>
+				</RigidBody>
+				<RigidBody
+					colliders={false}
+					position={[0, 1, 0]}
+					rotation={[Math.PI * 0.5, 0, 0]}
+				>
+					<CuboidCollider args={[1.5, 1.5, 0.5]} />
+					<CuboidCollider
+						args={[0.25, 1, 0.25]}
+						position={[0, 0, 1]}
+						rotation={[-Math.PI * 0.35, 0, 0]}
+					/>
+					<mesh castShadow>
+						<torusGeometry args={[1, 0.5, 16, 32]} />
+						<meshStandardMaterial color="mediumpurple" />
+					</mesh>
+				</RigidBody>
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+				<RigidBody type="fixed">
+					<mesh receiveShadow position-y={-1.25}>
+						<boxGeometry args={[10, 0.5, 10]} />
+						<meshStandardMaterial color="greenyellow" />
+					</mesh>
+				</RigidBody>
+			</Physics>
+		</>
+	);
 }
---
+++ portfolio/src/Experience.jsx
@@ -0,0 +1,61 @@
+import {
+	PresentationControls,
+	Text,
+	useGLTF,
+	Html,
+	Environment,
+	Float,
+	ContactShadows,
+} from "@react-three/drei";
+
+export default function Experience() {
+	const computer = useGLTF(
+		"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+	);
+
+	return (
+		<>
+			<Environment preset="city" />
+			<color args={["#241a1a"]} attach="background" />
+			<PresentationControls
+				global
+				rotation={[0.13, 0.1, 0]}
+				polar={[-0.4, 0.2]}
+				azimuth={[-1, 0.75]}
+				config={{ mass: 2, tension: 400 }}
+				snap
+			>
+				<Float rotationIntensity={0.4}>
+					<rectAreaLight
+						width={2.5}
+						height={1.65}
+						intensity={65}
+						color={"#ff6900"}
+						rotation={[0.1, Math.PI, 0]}
+						position={[0, 0.55, -1.15]}
+					/>
+					<primitive object={computer.scene} position-y={-1.2}>
+						<Html
+							transform
+							wrapperClass="htmlScreen"
+							distanceFactor={1.17}
+							position={[0, 1.56, -1.4]}
+							rotation-x={-0.256}
+						>
+							<iframe src="" />
+						</Html>
+					</primitive>
+					<Text rotation={[2, 0.75, 0.75]} position={[2, 0.75, 0.75]}>
+						Portfolio
+					</Text>
+				</Float>
+			</PresentationControls>
+			<ContactShadows
+				position-y={-1.4}
+				opacity={0.4}
+				scale={5}
+				blur={2.4}
+			/>
+		</>
+	);
+}
