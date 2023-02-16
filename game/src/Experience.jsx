11aaef67e040bcd7030fe3f65b7e5477a25943ff
Thabish <kadertabish2@gmail.com>
2023-02-16 13:57:59 +0400

    ground-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- game/src/Experience.jsx
+++ game/src/Experience.jsx
@@ -1,14 +1,16 @@
 import { OrbitControls } from "@react-three/drei";
 import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
-
+import { Physics, Debug } from "@react-three/rapier";
 export default function Experience() {
 	return (
 		<>
 			<OrbitControls makeDefault />
-
-			<Lights />
-			<Level />
+			<Physics>
+				<Debug />
+				<Lights />
+				<Level />
+			</Physics>
 		</>
 	);
 }
--- game/src/Level.jsx
+++ game/src/Level.jsx
@@ -1,25 +1,25 @@
+import * as THREE from "three";
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+function BlockStart({ position = [0, 0, 0] }) {
+	return (
+		<group position={position}>
+			<mesh
+				position={[0, -0.1, 0]}
+				geometry={boxGeometry}
+				scale={[4, 0.2, 4]}
+				receiveShadow
+			>
+				<meshStandardMaterial color="limegreen" />
+			</mesh>
+		</group>
+	);
+}
+
 export const Level = () => {
 	return (
 		<>
-			<mesh castShadow position-x={-2}>
-				<sphereGeometry />
-				<meshStandardMaterial color="orange" />
-			</mesh>
-
-			<mesh castShadow position-x={2} scale={1.5}>
-				<boxGeometry />
-				<meshStandardMaterial color="mediumpurple" />
-			</mesh>
-
-			<mesh
-				receiveShadow
-				position-y={-1}
-				rotation-x={-Math.PI * 0.5}
-				scale={10}
-			>
-				<planeGeometry />
-				<meshStandardMaterial color="greenyellow" />
-			</mesh>
+			<BlockStart position={[0, 0, 0]} />
 		</>
 	);
 };
